ced1d457114b5a84cdc154b292f805917ef98a58
Colby Birkhead <birkheadc@gmail.com>
2023-12-31 21:48:48 +0900

    split seed data into dev and prod

--- fullStack/nestJS_react/backend/serverless/seed.ts
+++ fullStack/nestJS_react/backend/serverless/seed.ts
@@ -1,10 +1,14 @@
-import * as seedData from './seedData.json';
+import { randomUUID } from 'crypto';
+import * as seedData from './seedData.prod.json';
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { hashSync } from 'bcrypt';
 
 (async function seed() {
   const region = process.argv[0];
   const client = new DynamoDBClient({ region: region });
   seedData.forEach(async (element) => {
+    element.id.S = randomUUID();
+    element.password.S = hashSync(element.password.S, 10);
     const command = new PutItemCommand({
       TableName: 'nextjsreacttemplateUsers',
       Item: element
