7db2ea8c37f5f261184f47112dd4cebab50f1fdc
Colby Birkhead <birkheadc@gmail.com>
2024-01-24 23:44:22 +0900

    change parallax icon

--- frontEnd/next/next-basic-template/src/components/nav/navLinks/NavLinks.tsx
+++ frontEnd/next/next-basic-template/src/components/nav/navLinks/NavLinks.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import NavLink from './navLink/NavLink';
-import { CameraIcon, EyeDropperIcon, FilmIcon, HomeIcon, MapIcon, MapPinIcon, SwatchIcon } from '@heroicons/react/24/outline';
+import { CameraIcon, EyeDropperIcon, FilmIcon, HomeIcon, MapIcon, MapPinIcon, SparklesIcon, SwatchIcon } from '@heroicons/react/24/outline';
 
 type NavLinksProps = {
 
@@ -12,7 +12,7 @@ export default function NavLinks(props: NavLinksProps): JSX.Element {
       <NavLink id='hero'><HomeIcon height={'100%'}/> <span className='hidden lg:inline-block'>home</span></NavLink>
       <NavLink id='nav-brag'><MapIcon height={'100%'} /> <span className='hidden lg:inline-block'>navigation</span></NavLink>
       <NavLink id='colors-generator'><EyeDropperIcon height={'100%'} /> <span className='hidden lg:inline-block'>colors</span></NavLink>
-      <NavLink id='parallax'><FilmIcon height={'100%'} /> <span className='hidden lg:inline-block'>parallax</span></NavLink>
+      <NavLink id='parallax'><SparklesIcon height={'100%'} /> <span className='hidden lg:inline-block'>parallax</span></NavLink>
     </ul>
   );
 }
