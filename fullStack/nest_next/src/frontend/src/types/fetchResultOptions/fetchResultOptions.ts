424855cf9f55bd5a7f6a32f678f996068d1bd135
Colby Birkhead <birkheadc@gmail.com>
2024-02-25 21:35:13 +0900

    fix bug in FetchResultOptions having incorrect type

--- fullStack/nest_next/src/frontend/src/types/fetchResultOptions/fetchResultOptions.ts
+++ fullStack/nest_next/src/frontend/src/types/fetchResultOptions/fetchResultOptions.ts
@@ -3,6 +3,6 @@ import { ResultMessage } from "../result/resultMessage"
 export type FetchResultOptions<T> = {
   route: string,
   builder?: (response: Response) => Promise<T>,
-  successMessage?: ResultMessage | string,
+  successMessage?: ResultMessage,
   init?: RequestInit
 }
