9e03de2042c9bdd7ce39d30b20fb98ae1419e26c
Colby Birkhead <birkheadc@gmail.com>
2024-07-01 15:32:30 +0900

    create automatic testing with storybook

--- fullStack/vite_aspnetcore_template/apps/vite_aspnetcore_template_front/src/components/nav/PrimaryNav/PrimaryNav.stories.ts
+++ fullStack/vite_aspnetcore_template/apps/vite_aspnetcore_template_front/src/components/nav/PrimaryNav/PrimaryNav.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within, expect } from "@storybook/test";
 import PrimaryNav from "./PrimaryNav";
 
 const meta: Meta<typeof PrimaryNav> = {
@@ -19,5 +20,26 @@ export const SmallScreen: Story = {
     viewport: {
       defaultViewport: "mobileVertical",
     },
+  },
+};
+
+export const OpenLeftPanel: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobileVertical",
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement as HTMLCanvasElement);
+    const menuButton = canvas.getByLabelText("open navigation menu");
+
+    await userEvent.click(menuButton);
+
+    const NUM_LINKS = 3;
+    for (let i = 0; i < NUM_LINKS + 1; i++) {
+      await userEvent.tab();
+    }
+
+    await expect(menuButton.matches(":focus")).toBeTruthy();
   },
 };
